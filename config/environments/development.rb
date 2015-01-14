60433a85b3cc631a3d41346e83e99bf4fc2e2237
Andreas Hubenthal <ibu2002@gmail.com>
2015-01-14 11:33:12 +0000

    fix config bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
     }
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
-  config.action.mailer.delivery_method = :smtp
-  config.action.mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
